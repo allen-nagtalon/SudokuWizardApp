2293b69eb45b7658cb31aa9a9bc8e6833b42cd53
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-01-11 17:38:01 -0800

    Implemented recursive solve function

--- app/src/main/java/com/example/sudokuWizard/SudokuSolver.kt
+++ app/src/main/java/com/example/sudokuWizard/SudokuSolver.kt
@@ -1,18 +1,39 @@
 package com.example.sudokuWizard
 
 class SudokuSolver(private val boardLayout : String) {
+    private val EMPTY_CELL = 0
+    private val MIN_VALUE = 1
+    private val MAX_VALUE = 9
+    private val START_INDEX = 0
+    private val END_INDEX = 8
+
     // Split board layout string into individual cells
     private val cells = boardLayout.split(",").toTypedArray()
 
     // Initialize board as 2D array of Int
     private var board = Array(9) { i -> IntArray(9) { j -> cells[(i*9)+j].toInt()} }
 
-    fun solve() {
-
+    // Recursive function for backtracking algorithm to traverse all empty cells
+    fun solve() : Boolean {
+        for(row in START_INDEX..END_INDEX) {
+            for(col in START_INDEX..END_INDEX) {
+                if(board[row][col] == EMPTY_CELL) {
+                    for(value in MIN_VALUE..MAX_VALUE) {
+                        board[row][col] = value
+                        if(isValid(row, col) && solve()) {
+                            return true
+                        }
+                        board[row][col] = EMPTY_CELL
+                    }
+                    return false
+                }
+            }
+        }
+        return true
     }
 
     // Checks the validity of a given cell in the board against the constraints of Sudoku
-    fun isValid(row : Int, col : Int) : Boolean {
+    private fun isValid(row : Int, col : Int) : Boolean {
         return rowCheck(row) && colCheck(col) && boxCheck(row, col)
     }
 
@@ -20,7 +41,7 @@ class SudokuSolver(private val boardLayout : String) {
     private fun rowCheck(row : Int) : Boolean {
         val rowList = ArrayList<Int>()
         for(col in board[row]) {
-            if(col > 0) rowList.add(col)
+            if(col != EMPTY_CELL) rowList.add(col)
         }
         return rowList.distinct().size == rowList.size
     }
@@ -29,7 +50,7 @@ class SudokuSolver(private val boardLayout : String) {
     private fun colCheck(col : Int) : Boolean {
         val colList = ArrayList<Int>()
         for(row in board) {
-            if(row[col] > 0) colList.add(row[col])
+            if(row[col] != EMPTY_CELL) colList.add(row[col])
         }
         return colList.distinct().size == colList.size
     }
@@ -42,7 +63,7 @@ class SudokuSolver(private val boardLayout : String) {
 
         for(i in boxRow..(boxRow+2)) {
             for(j in boxCol..(boxCol+2)) {
-                if(board[i][j] > 0) boxList.add(board[i][j])
+                if(board[i][j] != EMPTY_CELL) boxList.add(board[i][j])
             }
         }
 
@@ -53,7 +74,7 @@ class SudokuSolver(private val boardLayout : String) {
     fun printBoard() {
         for(row in board) {
             for(col in row) {
-                if (col > 0) print("$col ") else print("- ")
+                if (col != EMPTY_CELL) print("$col ") else print("- ")
             }
             println()
         }
@@ -62,14 +83,24 @@ class SudokuSolver(private val boardLayout : String) {
 
 fun main(args : Array<String>) {
     val s = SudokuSolver(
-            "8,4,7,5,0,9,6,0,2," +
-            "0,0,0,0,0,0,6,0,0," +
-            "0,0,0,0,8,0,0,0,9," +
-            "0,7,0,2,0,0,0,0,3," +
-            "0,0,9,0,7,0,5,0,0," +
-            "1,0,0,0,0,5,0,7,0," +
-            "2,0,0,0,5,0,0,0,0," +
-            "0,0,8,0,0,0,0,0,0," +
-            "6,3,0,3,0,4,8,1,7")
+            "8,4,7,5,0,9,0,0,2," +
+                    "0,0,0,0,0,0,6,0,0," +
+                    "0,0,0,0,8,0,0,0,9," +
+                    "0,7,0,2,0,0,0,0,3," +
+                    "0,0,9,0,7,0,5,0,0," +
+                    "1,0,0,0,0,5,0,7,0," +
+                    "2,0,0,0,5,0,0,0,0," +
+                    "0,0,8,0,0,0,0,0,0," +
+                    "6,0,0,3,0,4,8,1,7")
+
+    println("========================")
+
     s.printBoard()
+
+    println("========================")
+
+    if(s.solve()) { println("Solved.") } else { println("Not solved.") }
+    s.printBoard()
+
+    println("========================")
 }
