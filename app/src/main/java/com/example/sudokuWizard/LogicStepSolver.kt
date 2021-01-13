39fdd7cc25d89fc4b8ac52d4c529fa16af55d22a
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-01-13 12:54:51 -0800

    Created LogicStepSolver.kt as another solver to implement SudokuSolver.kt

---
+++ app/src/main/java/com/example/sudokuWizard/LogicStepSolver.kt
@@ -0,0 +1,11 @@
+package com.example.sudokuWizard
+
+class LogicStepSolver(private val boardLayout : String) : SudokuSolver {
+    override fun solve() : Boolean {
+        return false
+    }
+
+    override fun printBoard() {
+
+    }
+}
