5c146b020453e68ddb6e5c0b98361f4509bf8243
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-01-24 20:36:35 -0800

    Refactored SudokuBoard.kt to Board.kt Changed board storage to 2D Array of Cell instead of Int

---
+++ app/src/main/java/com/example/sudokuWizard/engine/Board.kt
@@ -0,0 +1,42 @@
+package com.example.sudokuWizard.engine
+
+import androidx.lifecycle.MutableLiveData
+
+class Board(
+    private val cells: List<Cell>,
+    val MAX_VALUE : Int,
+    val ROWS : Int,
+    val COLS : Int,
+    val ROW_SUB_LENGTH : Int,
+    val COL_SUB_LENGTH : Int) {
+
+    // General Properties
+    val EMPTY_CELL = 0
+    val START_INDEX = 0
+    val MIN_VALUE = 1
+
+    // Initialize board as 2D array of Int
+    var board = Array(ROWS) {
+            i -> Array(COLS) {
+            j -> cells[(i*COLS)+j] }
+    }
+
+    operator fun get(i : Int) = board[i]
+
+    fun getCell(row : Int, col : Int) = board[row][col]
+
+    fun printBoard() {
+        for(row in board) {
+            for(col in row) {
+                if (col.value != EMPTY_CELL) {
+                    if(col.value > 9) print("${(col.value+55).toChar()} ")
+                    else print("$col ")
+                }
+                else print("- ")
+            }
+            println()
+        }
+    }
+
+    constructor(cells : List<Cell>) : this(cells,9,9,9,3,3)
+}
