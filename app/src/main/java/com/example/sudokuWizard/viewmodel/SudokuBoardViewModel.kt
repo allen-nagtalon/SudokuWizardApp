4290036fab02649c421e3b304deb8e2ea7119228
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-01-26 14:11:07 -0800

    Deleted redundant custom ViewModel

--- app/src/main/java/com/example/sudokuWizard/viewmodel/SudokuBoardViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sudokuWizard.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.example.sudokuWizard.engine.SudokuBoard
-
-class SudokuBoardViewModel : ViewModel() {
-    val board = SudokuBoard()
-}
