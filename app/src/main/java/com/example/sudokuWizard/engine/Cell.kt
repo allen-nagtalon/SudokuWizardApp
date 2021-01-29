c2bd7b19f0b8cb3bbd16643cf26a3310e09eaa45
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-01-28 17:55:10 -0800

    Code clean-up for readability

--- app/src/main/java/com/example/sudokuWizard/engine/Cell.kt
+++ app/src/main/java/com/example/sudokuWizard/engine/Cell.kt
@@ -1,3 +1,8 @@
 package com.example.sudokuWizard.engine
 
-class Cell (val row : Int, val col : Int, var value : Int, var permanent : Boolean)
+class Cell (
+        val row : Int,
+        val col : Int,
+        var value : Int,
+        var permanent : Boolean = false
+)
