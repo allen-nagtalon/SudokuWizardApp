721a42fb3edf16c663077ba55bb84f530b1c67b9
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-02-01 15:27:56 -0800

    Added new activity for main menu

---
+++ app/src/main/java/com/example/sudokuWizard/view/MainMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sudokuWizard.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sudokuWizard.R
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
