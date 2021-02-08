9232f1fa38f10dd35c6324f181d165d986cf1add
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-02-08 14:15:51 -0800

    Implemented permission check for camera usage

--- app/src/main/java/com/example/sudokuWizard/view/ScanBoardActivity.kt
+++ app/src/main/java/com/example/sudokuWizard/view/ScanBoardActivity.kt
@@ -1,12 +1,38 @@
 package com.example.sudokuWizard.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+
 import com.example.sudokuWizard.R
 
+
 class ScanBoardActivity : AppCompatActivity() {
+    private val RC_HANDLE_CAMERA_PERM = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_board)
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startTextRecognizer()
+        } else {
+            askCameraPermission()
+        }
+    }
+
+    private fun startTextRecognizer() {
+
+    }
+
+    private fun askCameraPermission() {
+        val permissions = Array(1) {i -> Manifest.permission.CAMERA}
+
+        if(!ActivityCompat.shouldShowRequestPermissionRationale(this,
+            Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM)
+        }
     }
 }
