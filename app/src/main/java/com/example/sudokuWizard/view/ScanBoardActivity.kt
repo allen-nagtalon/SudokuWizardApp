5e5a7df6cfb7c190173f5539b3fafdf1dcfe6f99
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-02-10 15:03:16 -0800

    Adjusted permissions request Added CameraX preview setup Started TextScanAnalyzer code

--- app/src/main/java/com/example/sudokuWizard/view/ScanBoardActivity.kt
+++ app/src/main/java/com/example/sudokuWizard/view/ScanBoardActivity.kt
@@ -4,35 +4,93 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.example.sudokuWizard.R
+import com.google.mlkit.vision.common.InputImage
+import kotlinx.android.synthetic.main.activity_scan_board.*
 
 
 class ScanBoardActivity : AppCompatActivity() {
-    private val RC_HANDLE_CAMERA_PERM = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_board)
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            startTextRecognizer()
+        if(allPermissionsGranted()) {
+            startCamera()
         } else {
-            askCameraPermission()
+            ActivityCompat.requestPermissions(
+                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
     }
 
-    private fun startTextRecognizer() {
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
+        cameraProviderFuture.addListener(Runnable {
+            // Bind lifecycle camera to that of owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Build CameraX PreviewView
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(view_finder.createSurfaceProvider())
+                }
+
+            // Build CameraX Analyzer
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    // SET ANALYZER HERE
+                }
+
+            // Select back camera
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 
-    private fun askCameraPermission() {
-        val permissions = Array(1) {i -> Manifest.permission.CAMERA}
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+                baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
 
-        if(!ActivityCompat.shouldShowRequestPermissionRationale(this,
-            Manifest.permission.CAMERA)) {
-            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM)
+    /**
+    private class TextScanAnalyzer : ImageAnalysis.Analyzer {
+
+        override fun analyze(imageProxy: ImageProxy) {
+            val mediaImage = imageProxy.image
+            if(mediaImage != null) {
+                val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            }
         }
+    } **/
+
+    companion object {
+        private const val TAG = "CameraX"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
