e38153af76d379e1dbb8a57542174ee0168cfa47
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-04-01 16:00:24 -0700

    Implemented conversion from analysis to board overlay

--- app/src/main/java/com/example/sudokuWizard/view/customview/BoardOverlayView.kt
+++ app/src/main/java/com/example/sudokuWizard/view/customview/BoardOverlayView.kt
@@ -1,9 +1,7 @@
 package com.example.sudokuWizard.view.customview
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
@@ -13,25 +11,66 @@ import kotlin.math.min
 class BoardOverlayView(context : Context,
                        attributeSet: AttributeSet) : View(context, attributeSet) {
 
-    private var cellPixelSize = 0F
+    private var cellPixelSize = 0
+
+    private var xPadding = 0
+    private var yPadding = 0
 
     private var rows = 9
     private var cols = 9
     private var rowSubSize = 3
     private var colSubSize = 3
 
+    private var board = Array(rows) {
+        _ -> Array(cols) {
+        _ -> 0 }
+    }
+
+    private val permanentCellTextPaint = Paint().apply {
+        style = Paint.Style.FILL_AND_STROKE
+        color = Color.CYAN
+        textSize = 64F
+        typeface = Typeface.DEFAULT_BOLD
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
         // Set height and width of the SudokuBoardView to be the min of the two dimensions
-        val size = min(widthMeasureSpec - 64, heightMeasureSpec - 64)
+        val size = min(widthMeasureSpec - 64 - (widthMeasureSpec % 9),
+            heightMeasureSpec- 64 - (heightMeasureSpec % 9))
+        xPadding = (widthMeasureSpec - size) / 2
+        yPadding = (heightMeasureSpec - size) / 2
         setMeasuredDimension(size, size)
 
         /** Adjust this later to account for varying sized boards. **/
     }
 
     override fun onDraw(canvas : Canvas) {
-        cellPixelSize = (width / rows).toFloat()
+        cellPixelSize = (width / rows)
+
+        drawScans(canvas)
+    }
+
+    private fun drawScans(canvas: Canvas) {
+        for(i in board.indices) {
+            for(j in board[i].indices) {
+                if(board[i][j] != 0) {
+                    val valString = board[i][j].toString()
+                    val textBounds = Rect()
+
+                    permanentCellTextPaint.getTextBounds(valString, 0, valString.length, textBounds)
+
+                    val textWidth = permanentCellTextPaint.measureText(valString)
+                    val textHeight = textBounds.height()
+
+                    canvas.drawText(valString,
+                        (j * cellPixelSize) + cellPixelSize / 2 - textWidth / 2,
+                        (i * cellPixelSize) + cellPixelSize / 2 + textHeight / 2F,
+                        permanentCellTextPaint)
+                }
+            }
+        }
     }
 
     fun processScan(results: Text) {
@@ -39,9 +78,19 @@ class BoardOverlayView(context : Context,
             for(line in block.lines) {
                 for(element in line.elements) {
                     // Check if the element is a single digit
-                    Log.d("SCANBOARD", "${element.text}: ${element.boundingBox}")
+                    if(element.text.length == 1 && element.text[0].isDigit()) {
+                        val bounds = element.boundingBox
+                        if(bounds != null) {
+                            val possibleX = (bounds.centerX() - xPadding) / 330
+                            val possibleY = (bounds.centerY() - yPadding) / 330
+                            Log.d("SCANBOARD", "\"${element.text}\" at ($possibleX, $possibleY).")
+                            if(possibleX < 9 && possibleY < 9)
+                                board[possibleY][possibleX] = element.text.toInt()
+                        }
+                    }
                 }
             }
         }
+        invalidate()
     }
 }
