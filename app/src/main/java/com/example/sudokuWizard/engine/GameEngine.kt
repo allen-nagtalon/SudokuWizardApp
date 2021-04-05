991d5db3bb3c6df4ee63552f669b93175299b2f8
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-04-05 14:37:56 -0700

    Added boardChange function to import a boardLayout string

--- app/src/main/java/com/example/sudokuWizard/engine/GameEngine.kt
+++ app/src/main/java/com/example/sudokuWizard/engine/GameEngine.kt
@@ -16,7 +16,7 @@ class GameEngine(val rows : Int,
     private var selectedRow = -1
     private var selectedCol = -1
 
-    private val board : Board
+    private var board : Board
 
     init {
         // Split board layout string into individual cells
@@ -34,6 +34,20 @@ class GameEngine(val rows : Int,
         selectedCellLiveData.postValue(Pair(selectedRow, selectedCol))
         cellsLiveData.postValue(board.board)
         boardEditEnabled = false
+    }
+
+    fun changeBoard(boardLayout: String) {
+        val cellValues = boardLayout.split(",").toTypedArray()
+
+        val cells = List(rows*cols) {i ->
+            when(cellValues[i].toInt()) {
+                0 -> Cell(i / cols, i % cols, 0)
+                else -> Cell(i / cols, i % cols, cellValues[i].toInt(), true)
+            }
+        }
+
+        board = Board(cells)
+        cellsLiveData.postValue(board.board)
     }
 
     fun handleInput(number : Int) {
@@ -104,18 +118,6 @@ class GameEngine(val rows : Int,
         }
     }
 
-    fun updateBoardSettings(rows : Int, cols : Int, rowSubSize : Int, colSubSize : Int) {
-
-    }
-
     constructor() : this(9, 9, 3, 3,
-        "8,4,7,5,0,9,0,0,2," +
-            "0,0,0,0,0,0,6,0,0," +
-            "0,0,0,0,8,0,0,0,9," +
-            "0,7,0,2,0,0,0,0,3," +
-            "0,0,9,0,7,0,5,0,0," +
-            "1,0,0,0,0,5,0,7,0," +
-            "2,0,0,0,5,0,0,0,0," +
-            "0,0,8,0,0,0,0,0,0," +
-            "6,0,0,3,0,4,8,1,7")
+    "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
 }
